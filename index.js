1581bc1c62628b79a1b4e4b63fae7e78af8cd775
cadenchilders2-cmyk <cadenchilders2@gmail.com>
2026-03-05 15:15:14 -0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-{
-  "short_name": "30Min Fitness",
-  "name": "30 Minutes Fitness",
-  "start_url": ".",
-  "display": "standalone",
-  "theme_color": "#f97316",
-  "background_color": "#080810",
-  "icons": []
-}
